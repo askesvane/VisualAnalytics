e917c31e14ab42f9a2f3f77ddf873dca5b3d14da
askesvane <askesvane@gmail.com>
2021-03-05 21:05:11 +0100

    small venv changes

--- assignment3_EdgeDetection/create_visual_venv.sh
+++ assignment3_EdgeDetection/create_visual_venv.sh
@@ -6,12 +6,15 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
+# problems when installing from requirements.txt
 pip install ipython
 pip install jupyter
+pip install matplotlib
+pip install opencv-python
+
 python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements.txt && pip install -r requirements.txt
-python -m spacy download en_core_web_sm
+test -f requirements.txt && pip install requirements.txt
 
 deactivate
 echo "build $VENVNAME"
